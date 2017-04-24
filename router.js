025f59e782f8ad0d7635543be98dacf99a5fe5df
sandhyalupin <lsandhya.05@gmail.com>
2017-04-24 16:05:33 +0530

    Add files via upload

---
+++ router.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('home');
+  this.route('help');
+  this.route('questions', function() {
+  this.route('question', { path: ':question_id'} );
+});
+
+export default Router;
