73e2d2e3b7c861ba6270a7d195d61c14543abbb7
sandhyalupin <lsandhya.05@gmail.com>
2017-04-27 15:35:50 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'testapp',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/codersandy',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
