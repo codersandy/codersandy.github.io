da73675500f438793785c4640feb38c4c2e82583
sandhyalupin <lsandhya.05@gmail.com>
2017-05-02 14:53:07 +0530

    UI changes

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	actions: {
-		saveUser() {
-			sessionStorage.setItem('name', this.get('firstName'));
-			sessionStorage.setItem('totalScore',0);
-			sessionStorage.setItem('answered',0);
-			console.log(this.get('firstName'));
-			this.transitionToRoute('questions.q1',1);
-		}}
+  actions: {
+    saveUser() {
+      sessionStorage.setItem('name', this.get('firstName'));
+      sessionStorage.setItem('totalScore', 0);
+      sessionStorage.setItem('answered', 0);
+      console.log(this.get('firstName'));
+      this.transitionToRoute('questions.q1', 1);
+    },
+    gotoHelp() {
+      this.transitionToRoute('help');
+    }
+  }
 
 });
